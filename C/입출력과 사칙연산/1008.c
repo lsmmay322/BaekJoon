01309b42fd39cc275e6fc7403f0d6519ba5622f9
hwanseob.lee <lsmmay322@naver.com>
2024-02-25 15:45:23 +0900

    [add] C_입출력

---
+++ C/입출력과 사칙연산/1008.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	double x, y;
+	scanf("%lf %lf", &x, &y);
+	printf("%.9f", x/y);
+}
---
+++ C/입출력과 사칙연산/11382.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+	long x, y, z;
+
+	scanf("%ld %ld %ld", &x, &y, &z);
+	printf("%ld", x+y+z);
+}
